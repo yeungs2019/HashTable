1521c59039a6eb0d10a89360b5adf1acf2ff80b8
yeungs2019 <sayeung@csustudent.net>
2020-01-28 19:56:13 -0500

    hypos.pl

--- lab03/hypos.pl
+++ lab03/hypos.pl
@@ -1,6 +1,22 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl 
 
 use strict;
 use warnings;
 
 # put your code here :-) 
+# declaration of variables
+print "Enter some sides of a triangle!\n";
+print "Enter a side: ";
+my $opposite = <STDIN>;
+chomp $opposite;
+
+print "Enter a side: ";
+my $adjacent = <STDIN>;
+chomp $adjacent;
+
+my $thecombinedsides = (($opposite**2) + ($adjacent**2));
+my $hypotenuse = sqrt($thecombinedsides);
+printf("The length of the hypotenuse is: %.2f\n", $hypotenuse)
+
+# print "The hypotenuse length is $hypotenuse\n";
+
